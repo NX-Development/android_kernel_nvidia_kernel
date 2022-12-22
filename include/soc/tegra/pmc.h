6fb6f51d37902d32e5fbc81399823077e6d4a0a2
CTCaer <ctcaer@gmail.com>
2022-12-22 15:34:18 +0200

    soc: tegra: pmc: update brom psu commands driver
    - Fix existing bugs
    - Add live custom command update support

--- include/soc/tegra/pmc.h
+++ include/soc/tegra/pmc.h
@@ -375,4 +375,16 @@ void tegra_pmc_writel_relaxed(u32 value, unsigned long offset);
 u32 tegra_pmc_readl(unsigned long offset);
 void tegra_pmc_writel(u32 value, unsigned long offset);
 
+struct tegra_br_cmd_cfg {
+	u32 dev;
+	u32 idx;
+	u32 val;
+};
+
+int tegra_pmc_edit_bootrom_scratch_poff(struct tegra_br_cmd_cfg *bcfg,
+					u32 bcfg_size);
+int tegra_pmc_edit_bootrom_scratch_reset(struct tegra_br_cmd_cfg *bcfg,
+					u32 bcfg_size);
+void tegra_pmc_r2p_setup(const char *cmd, bool panic_occurred);
+
 #endif /* __SOC_TEGRA_PMC_H__ */
