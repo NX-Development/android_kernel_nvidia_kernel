OE4T/linux-tegra-4.9

dmaengine: tegra210-adma: Wrong address used for interrupt register
The address used for global interrupt clear register update on Tegra210 is
incorrect. The tdma_global_ch_write() does not consider the global offset
address and current offset for global interrupt register does not take
this into account. Because of this register update is made at '0x20'
instead of '0xc20'.

On Tegra186 and later, ADMA channels are virtualised and aliased into 4 64K
pages. A separate page carries global and configuration registers for ADMA.
Few registers are reshuffled as part of it and moved to page specific space.
Thus offset of these registers are relative to the channel base offset and
it needs to be taken into account while updating. In summary the register
space looks like below:

- Tegra210
     |
     |--> channel registers
     |		...
     |--> global registers
     |

- Tegra186
     |
     |--> global registers
     |		...
     |--> page-1 channel registers
     |		...
     |--> page-1 global registers
     |
     |		...
     |
     |--> page-4 channel registers
     |		...
     |--> page-4 global registers
     |

The global interrupt register on Tegra210 is part of global space where as
on Tegra186 (and later) it belongs to page specific global space. Existing
issue can be fixed by using tdma_global_write() instead and taking channel
offset into account. In doing so tdma_global_ch_write() can be removed.

Bug 200664413

Change-Id: I14a810db544cb8b9444144d0bdb527636422887e
Reviewed-on: https://git-master.nvidia.com/r/c/linux-5.9/+/2424007
(manually cherry picked from commit 0babcd1fe17935a295c0322cf48519c6cbd2d2d4)
Signed-off-by: Sameer Pujar <spujar@nvidia.com>
Reviewed-on: https://git-master.nvidia.com/r/c/linux-4.9/+/2427206
(cherry picked from commit d3c0823042b132b88f55e1a01888e785dd0aa588)
Reviewed-on: https://git-master.nvidia.com/r/c/linux-4.9/+/2430296
Reviewed-by: automaticguardword <automaticguardword@nvidia.com>
Reviewed-by: Mohan Kumar D <mkumard@nvidia.com>

drivers/dma/tegra210-adma.c
@@ -226,12 +226,6 @@ static inline u32 tdma_global_read(struct tegra_adma *tdma, u32 reg)
 	return readl(tdma->base_addr + global_reg_offset + reg);
 }
 
-static inline void tdma_global_ch_write(struct tegra_adma *tdma, u32 reg,
-					u32 val)
-{
-	writel(val, tdma->base_addr + tdma->ch_base_offset + reg);
-}
-
 static inline void tdma_ch_write(struct tegra_adma_chan *tdc, u32 reg, u32 val)
 {
 	writel(val, tdc->chan_addr + reg);
@@ -281,9 +275,20 @@ static int tegra_adma_init(struct tegra_adma *tdma)
 	unsigned int global_reg_offset = tdma->chip_data->global_reg_offset;
 	unsigned int reg_soft_reset;
 
-
-	/* Clear any interrupts */
-	tdma_global_ch_write(tdma, chip_data->global_int_clear, 0x1);
+	/*
+	 * Clear any interrupts:
+	 *
+	 * On Tegra186 and later, ADMA channels are virtualized and aliased
+	 * into 4 64K pages. A separate page carries global and configuration
+	 * registers for ADMA. Few registers are reshuffled as part of it and
+	 * moved to page specific space. Thus offset of these registers are
+	 * relative to the channel base offset and it needs to be taken into
+	 * account while updating. It works for Tegra210 as well as channel
+	 * base offset is 0.
+	 */
+	tdma_global_write(tdma,
+		tdma->ch_base_offset + chip_data->global_int_clear,
+		0x1);
 
 	if (tdma->is_virt == false) {
 		/* Assert soft reset */

NX-Development/android_kernel_nvidia_kernel

drivers/dma/tegra210-adma.c
@@ -226,12 +226,6 @@ static inline u32 tdma_global_read(struct tegra_adma *tdma, u32 reg)
 	return readl(tdma->base_addr + global_reg_offset + reg);
 }
 
-static inline void tdma_global_ch_write(struct tegra_adma *tdma, u32 reg,
-					u32 val)
-{
-	writel(val, tdma->base_addr + tdma->ch_base_offset + reg);
-}
-
 static inline void tdma_ch_write(struct tegra_adma_chan *tdc, u32 reg, u32 val)
 {
 	writel(val, tdc->chan_addr + reg);
@@ -281,9 +275,20 @@ static int tegra_adma_init(struct tegra_adma *tdma)
 	unsigned int global_reg_offset = tdma->chip_data->global_reg_offset;
 	unsigned int reg_soft_reset;
 
-
-	/* Clear any interrupts */
-	tdma_global_ch_write(tdma, chip_data->global_int_clear, 0x1);
+	/*
+	 * Clear any interrupts:
+	 *
+	 * On Tegra186 and later, ADMA channels are virtualized and aliased
+	 * into 4 64K pages. A separate page carries global and configuration
+	 * registers for ADMA. Few registers are reshuffled as part of it and
+	 * moved to page specific space. Thus offset of these registers are
+	 * relative to the channel base offset and it needs to be taken into
+	 * account while updating. It works for Tegra210 as well as channel
+	 * base offset is 0.
+	 */
+	tdma_global_write(tdma,
+		chip_data->ch_base_offset + chip_data->global_int_clear,
+		0x1);
 
 	if (tdma->is_virt == false) {
 		/* Assert soft reset */